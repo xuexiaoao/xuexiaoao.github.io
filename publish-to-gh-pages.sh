b754e0ef43efc7b4b17153804fe464700f594244
xuexiaoao <xuexiaoaoooo@gmail.com>
2017-10-21 22:12:17 +0800

    修改token名称

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -18,4 +18,4 @@ git config user.email "xuexiaoaoooo@gmail.com"
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 
-git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
+git push --force --quiet "https://${Travis_Token}@${GH_REF}" master:master
